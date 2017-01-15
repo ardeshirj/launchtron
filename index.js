371fb00b44385a15e7621c1ec49010a9d344095b
ardeshirj <ardeshir.javaherchi@gmail.com>
2017-01-15 15:42:05 -0800

    Replace electron prebuilt with electron pkg

--- index.js
+++
@@ -1,9 +0,0 @@
-var buttons = document.querySelectorAll('.btn');
-
-for (i = 0; i < buttons.length; i++) {
-  buttons[i].onclick = pressedBtn;
-}
-
-function pressedBtn() {
-  console.log(this.id + ' btn called me!');
-}
