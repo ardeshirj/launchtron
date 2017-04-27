72d8661dd2157c5f1eef977079632fe253e596ff
ardeshirj <ardeshir.javaherchi@gmail.com>
2017-04-26 22:57:42 -0700

    Emit input data to an observable

--- midi.js
+++ midi.js
@@ -1,57 +1,24 @@
-var midi = null;
+const Rx = require('rxjs/Rx');
+
+let midi = null;
 
 navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
 
 function onMIDISuccess(midiAccess) {
-  console.log('MIDI ready!');
+  console.log('MIDI ready!', midiAccess);
   midi = midiAccess;
 
-  // listInputsAndOutputs();
-  monitorInput();
-  sendMessage();
-}
-
-function onMIDIFailure(msg) {
-  console.log('Failed to get MIDI access - ' + msg);
-}
-
-function listInputsAndOutputs() {
-  for (var entry of midi.inputs) {
-    var input = entry[1];
-    console.log("Input port [type:'" + input.type + "'] id:'" + input.id +
-      "' manufacturer:'" + input.manufacturer + "' name:'" + input.name +
-      "' version:'" + input.version + "'");
-  }
-
-  for (var entry of midi.outputs) {
-    var output = entry[1];
-    console.log("Output port [type:'" + output.type + "'] id:'" + output.id +
-      "' manufacturer:'" + output.manufacturer + "' name:'" + output.name +
-      "' version:'" + output.version + "'");
-  }
-}
-
-function onMIDIMessage(event) {
-  var str = 'MIDI message received at timestamp ' + event.timeStamp +
-    '[' + event.data.length + ' bytes]: ';
-
-  for (var i = 0; i < event.data.length; i++) {
-    str += '0x' + event.data[i].toString(16) + ' ';
-  }
-
-  console.log(str);
-}
-
-function monitorInput() {
-  // Each press/release is an input
-  midi.inputs.forEach(function (input) {
-    input.onmidimessage = onMIDIMessage;
+  const subject = new Rx.Subject();
+  midi.inputs.forEach(input => {
+    input.onmidimessage = (event) => {
+      // console.log(event);
+      subject.next(event.data);
+    }
   });
+
+  // subject.forEach(x => console.log(x));
 }
 
-function sendMessage() {
-  var message = [0x80, 0x70, 0x3F];
-  midi.outputs.forEach(function (output) {
-    output.send(message);
-  });
+function onMIDIFailure(error) {
+  console.log('Midi Failed', error);
 }
