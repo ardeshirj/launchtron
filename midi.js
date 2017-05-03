1f9872689868bf74c390f135fbb3f207f0d6ce21
ardeshirj <ardeshir.javaherchi@gmail.com>
2017-05-02 22:49:02 -0700

    Create midi data stream using observable

--- midi.js
+++ midi.js
@@ -1,36 +1,14 @@
 const Rx = require('rxjs/Rx');
 
-let midi = null;
+const portIds = [];
 
-navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
-
-function onMIDISuccess(midiAccess) {
-  console.log('MIDI ready!', midiAccess);
-  midi = midiAccess;
-
-  const subject = new Rx.Subject();
-  midi.inputs.forEach(input => {
-    input.onmidimessage = (event) => {
-      // console.log(event);
-      subject.next(event.data);
-    }
-  });
-
-  subject.forEach(input => {
-    midi.outputs.forEach(output => {
-      // console.log(input, output);
-      // Launchpad 3 index in data is presenting press/relaese
-      // via decimal numbers (127 for on & 0 for off)
-      // Set color zero on release...
-      if (input[2] === 0) {
-        output.send([input[0], input[1], 0x00]);
-      } else {
-        output.send([input[0], input[1], 0x3C]);
-      }
-    });
-  });
-}
-
-function onMIDIFailure(error) {
-  console.log('Midi Failed', error);
-}
+const stream = Rx.Observable
+  .fromPromise(navigator.requestMIDIAccess())
+  .map(midi => midi.inputs)
+  .map(inputs => {
+    inputs.forEach(input => portIds.push(input.id));
+    // For launchpad mini we only intreseted on first port
+    return inputs.get(portIds[0]);
+  })
+  .flatMap(input => Rx.Observable.fromEvent(input, 'midimessage'))
+  .map(messageEvent => messageEvent.data);
