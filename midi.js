7c85720100ae7c5fad0215d713ede40ed7dbf20a
ardeshirj <ardeshir.javaherchi@gmail.com>
2017-05-04 20:02:25 -0700

    Set midi as its own observable

--- midi.js
+++ midi.js
@@ -1,11 +1,16 @@
 const Rx = require('rxjs/Rx');
 
-const portIds = [];
-
-const stream = Rx.Observable
+const midi = Rx.Observable
   .fromPromise(navigator.requestMIDIAccess())
+  .catch(error => console.log(error));
+
+const state = midi
+  .flatMap(midi => Rx.Observable.fromEvent(midi, 'statechange'))
+
+const input = midi
   .map(midi => midi.inputs)
   .map(inputs => {
+    const portIds = [];
     inputs.forEach(input => portIds.push(input.id));
     // For launchpad mini we only intreseted on first port
     return inputs.get(portIds[0]);
